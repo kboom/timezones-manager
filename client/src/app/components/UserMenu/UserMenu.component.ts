916102c2409002a35c051fcd84ccb695912c75fb
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2017-08-24 20:39:31 +0200

    Displaying logged in menu if token present (logged in)

--- client/src/app/components/UserMenu/UserMenu.component.ts
+++ client/src/app/components/UserMenu/UserMenu.component.ts
@@ -4,16 +4,20 @@ import {AuthenticationService} from "../../services/authentication.service";
 @Component({
     selector: 'userMenu',
     template: `
-        <button md-button [mdMenuTriggerFor]="menu">Menu</button>
-        <md-menu #menu="mdMenu">
-            <button md-menu-item>Item 1</button>
-            <button md-menu-item>Item 2</button>
-        </md-menu>
+
+        <div *ngIf="this.authService.isAuthenticated() | async">
+            <button md-button [mdMenuTriggerFor]="menu">Menu</button>
+            <md-menu #menu="mdMenu">
+                <button md-menu-item>Item 1</button>
+                <button md-menu-item>Item 2</button>
+            </md-menu>
+        </div>
+        
     `,
 })
 export class UserMenuComponent implements OnInit {
 
-    constructor(private authenticationService: AuthenticationService) {}
+    constructor(private authService: AuthenticationService) {}
 
     public ngOnInit() {
         console.log('hello `Detail` component');
--- client/src/app/services/authentication.service.ts
+++ client/src/app/services/authentication.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/defer';
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor() { }
+    token: string;
+
+    constructor(private http: Http) {
+
+    }
+
+    isAuthenticated(): Observable<Boolean> {
+        return Observable.defer(() => Observable.of(!!this.token))
+    }
 
 }
